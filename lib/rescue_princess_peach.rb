8576ce662c9f7fb64b32b1b4e527396b8d16f154
Elizabeth Jimenez <lizliz123@gmail.com>
2018-05-25 04:29:50 +0000

    automatically backed up by learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -4,9 +4,9 @@ def rescue_princess_peach
 end
 
 
-status = 'hi'
+#status = 'hi'
 
 ## Comment the below back in to see method scope in action!
 
 rescue_princess_peach #runs the rescue princess peach method that is defined on line 1
-puts status
+#puts status
